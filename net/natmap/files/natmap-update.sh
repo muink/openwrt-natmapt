afcbee21482a0ff693d36fa4c7c3d1a2a8c71ec5
Anya Lin <hukk1996@gmail.com>
2023-08-24 16:32:42 +0800

    natmap: Fix hook script parameter is not defined
    Signed-off-by: Anya Lin <hukk1996@gmail.com>

--- net/natmap/files/natmap-update.sh
+++ net/natmap/files/natmap-update.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
+ip="$1"
+port="$2"
+ip4p="$3"
+inner_port="$4"
+protocol="$5"
+inner_ip="$6"
+shift 6
 
 . /usr/share/libubox/jshn.sh
 
 (
 	json_init
 	json_add_string sid "$SECTIONID"
-	json_add_string ip "$1"
-	json_add_int port "$2"
-	json_add_string ip4p "$3"
-	json_add_int inner_port "$4"
-	json_add_string protocol "$5"
-	json_add_string inner_ip "$6"
-	shift 6
+	json_add_string ip "$ip"
+	json_add_int port "$port"
+	json_add_string ip4p "$ip4p"
+	json_add_int inner_port "$inner_port"
+	json_add_string protocol "$protocol"
+	json_add_string inner_ip "$inner_ip"
 	json_dump > /var/run/natmap/$PPID.json
 )
 
 [ -n "${NOTIFY_SCRIPT}" ] && {
 	export -n NOTIFY_SCRIPT
-	exec "${NOTIFY_SCRIPT}" "$ip" "$port" "$ip4p" "$inner_port" "$protocol" "$inner_ip" "$sid" "$@"
+	exec "${NOTIFY_SCRIPT}" "$ip" "$port" "$ip4p" "$inner_port" "$protocol" "$inner_ip" "$SECTIONID" "$@"
 }
