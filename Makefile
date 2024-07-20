muink/openwrt-natmapt

commit 4b7e4191230388e0d278159a191c5e31c3bf3f4d

create a natmap-unique curl

Makefile
@@ -2,7 +2,7 @@ include $(TOPDIR)/rules.mk
 
 PKG_NAME:=natmapt
 PKG_VERSION:=20240603
-PKG_RELEASE:=1
+PKG_RELEASE:=2
 
 RAW_NAME:=natmap
 PKG_BUILD_DIR:=$(BUILD_DIR)/$(RAW_NAME)-$(PKG_VERSION)
@@ -32,6 +32,21 @@ MAKE_FLAGS += REV_ID="$(PKG_VERSION)"
 
 define Package/natmapt/conffiles
 /etc/config/natmap
+endef
+
+define Package/natmapt/postinst
+#!/bin/sh
+if [ -z "$$IPKG_INSTROOT" ]; then
+	# system
+	ln -sf "$$(command -v curl)" /usr/bin/natmap-curl
+fi
+exit 0
+endef
+
+define Package/natmapt/prerm
+#!/bin/sh
+rm -f "$$IPKG_INSTROOT/usr/bin/natmap-curl"
+exit 0
 endef
 
 define Package/natmapt/install

files/client/Deluge
@@ -3,13 +3,13 @@
 #
 # Client listen port refresh script
 #
-# depends curl jsonfilter
+# depends jsonfilter
 
 . /usr/lib/natmap/common.sh
 
 # http_request <cookie> <data>
 http_request() {
-	curl $retry --compressed -L -X POST \
+	$CURL $retry --compressed -L -X POST \
 		-b "$1" -H "Content-Type: application/json" \
 		-d "$2" \
 		--url ${scheme}://${ip}:${web_port}/json
@@ -17,7 +17,7 @@ http_request() {
 
 start() {
 	local retry='--connect-timeout 1 --retry 0'
-	local cookie="$(curl $retry --compressed -L -i \
+	local cookie="$($CURL $retry --compressed -L -i \
 		-H "Content-Type: application/json" \
 		-d '{"method": "auth.login", "params": ["'"${password}"'"], "id": 1}' \
 		--url ${scheme}://${ip}:${web_port}/json \

files/client/Transmission
@@ -3,18 +3,18 @@
 #
 # Client listen port refresh script
 #
-# depends curl
+# depends 
 
 . /usr/lib/natmap/common.sh
 
 start() {
 	local retry='--connect-timeout 1 --retry 0'
 	scheme='http' # only supports http
-	local sid="$(curl $retry -L \
+	local sid="$($CURL $retry -L \
 		-u ${username}:${password} \
 		--url ${scheme}://${ip}:${web_port}/transmission/rpc \
 		| sed 's|.*<code>||g;s|</code>.*||g')"
-	curl $retry -L -X POST \
+	$CURL $retry -L -X POST \
 		-H "$sid" -u ${username}:${password} \
 		-d '{"method":"session-set","arguments":{"peer-port":"'"${port}"'"}}' \
 		--url ${scheme}://${ip}:${web_port}/transmission/rpc

files/client/qBittorrent
@@ -3,23 +3,23 @@
 #
 # Client listen port refresh script
 #
-# depends curl
+# depends 
 
 . /usr/lib/natmap/common.sh
 
 start() {
 	local retry='--connect-timeout 1 --retry 0'
 	local self_signed='-k'
-	local cookie="$(curl $retry $self_signed -L -i \
+	local cookie="$($CURL $retry $self_signed -L -i \
 		-H "Referer: ${scheme}://${ip}:${web_port}" \
 		-d "username=${username}&password=${password}" \
 		--url ${scheme}://${ip}:${web_port}/api/v2/auth/login \
 		| grep -i set-cookie | sed -En 's|^.*(SID=\S+);.*|\1|p')"
-	curl $retry $self_signed -L -X POST \
+	$CURL $retry $self_signed -L -X POST \
 		-b "$cookie" \
 		-d 'json={"listen_port":"'"${port}"'"}' \
 		--url ${scheme}://${ip}:${web_port}/api/v2/app/setPreferences
-	curl $retry $self_signed -L -I \
+	$CURL $retry $self_signed -L -I \
 		--cookie "$cookie" \
 		--url ${scheme}://${ip}:${web_port}/api/v2/auth/logout
 }

files/common.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Copyright (C) 2023 muink https://github.com/muink
 #
-# depends jsonfilter
+# depends curl jsonfilter
+
+CURL="$(command -v natmap-curl)"
 
 # JSON_EXPORT <json>
 JSON_EXPORT() {

files/ddns/Cloudflare
@@ -3,7 +3,7 @@
 #
 # DDNS script
 #
-# depends curl jsonfilter
+# depends jsonfilter
 
 . /usr/lib/natmap/common.sh
 . /usr/share/libubox/jshn.sh
@@ -15,7 +15,7 @@ http_request() {
 	local method
 	[ -z "${id}" ] && method=POST || method=PUT # or PATCH
 
-	curl $retry -L -o /dev/null -X ${method} \
+	$CURL $retry -L -o /dev/null -X ${method} \
 		--url "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${id}" \
 		-H "Authorization: Bearer ${TOKEN}" \
 		-H 'Content-Type: application/json' \
@@ -24,15 +24,15 @@ http_request() {
 
 start() {
 	local retry='--connect-timeout 3 --retry 3'
-	#curl $retry -L -X GET \
+	#$CURL $retry -L -X GET \
 	#	--url "https://api.cloudflare.com/client/v4/user/tokens/verify" \
 	#	-H "Authorization: Bearer ${TOKEN}" \
 	#	-H "Content-Type:application/json" | jq
-	#curl $retry -L -X GET \
+	#$CURL $retry -L -X GET \
 	#	--url "https://api.cloudflare.com/client/v4/zones" \
 	#	-H "Authorization: Bearer ${TOKEN}" \
 	#	-H "Content-Type:application/json" | jq
-	local records="$(curl $retry -L -X GET \
+	local records="$($CURL $retry -L -X GET \
 		--url "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records" \
 		-H "Authorization: Bearer ${TOKEN}" \
 		-H 'Content-Type: application/json')"

files/notify/Pushbullet
@@ -3,13 +3,13 @@
 #
 # Port update notify script
 #
-# depends curl
+# depends 
 
 . /usr/lib/natmap/common.sh
 
 start() {
 	local retry='--connect-timeout 3 --retry 3'
-	curl $retry -L -o /dev/null -X POST \
+	$CURL $retry -L -o /dev/null -X POST \
 		-H "Access-Token: ${TOKEN}" \
 		-H 'Content-Type: application/json' \
 		-d '{"type":"note","body":"'"${text}"'","title":"'"${TITLE}"'"}' \

files/notify/Pushover
@@ -3,13 +3,13 @@
 #
 # Port update notify script
 #
-# depends curl
+# depends 
 
 . /usr/lib/natmap/common.sh
 
 start() {
 	local retry='--connect-timeout 3 --retry 3'
-	curl $retry -L -o /dev/null -X POST \
+	$CURL $retry -L -o /dev/null -X POST \
 		-H 'Content-Type: application/json' \
 		-d '{"token":"'"${TOKEN}"'","user":"'"${TO_ID}"'","device":"'"${DEVICE}"'","message":"'"${text}"'","title":"'"${TITLE}"'","html":"'"${HTML}"'"}' \
 		--url https://${custom_domain:-api.pushover.net}/1/messages.json

commit 9fce114d63724ceb005304cb20658ceb677f97ca

Fix the problem that natmap-curl is not generated by built-in application
Fixes: 4b7e41912303 create a natmap-unique curl

Makefile
@@ -2,7 +2,7 @@ include $(TOPDIR)/rules.mk
 
 PKG_NAME:=natmapt
 PKG_VERSION:=20240603
-PKG_RELEASE:=2
+PKG_RELEASE:=3
 
 RAW_NAME:=natmap
 PKG_BUILD_DIR:=$(BUILD_DIR)/$(RAW_NAME)-$(PKG_VERSION)
@@ -32,15 +32,6 @@ MAKE_FLAGS += REV_ID="$(PKG_VERSION)"
 
 define Package/natmapt/conffiles
 /etc/config/natmap
-endef
-
-define Package/natmapt/postinst
-#!/bin/sh
-if [ -z "$$IPKG_INSTROOT" ]; then
-	# system
-	ln -sf "$$(command -v curl)" /usr/bin/natmap-curl
-fi
-exit 0
 endef
 
 define Package/natmapt/prerm

files/natmap.defaults
@@ -27,3 +27,6 @@ else
 ## def_interval --> def_tcp_interval/def_udp_interval
 sed -Ei 's|def_interval (.*)|def_tcp_interval \1\n\toption def_udp_interval \1|' "$CONF"
 fi
+# gen natmap-curl
+ln -sf "$(command -v curl)" /usr/bin/natmap-curl
+exit 0